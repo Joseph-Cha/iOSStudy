846945f7c921003382c6e02f494556fac5a03a0b
Joseph.c <joseph.c@kakao.com>
2022-11-17 22:29:18 +0900

    #61 Dispatch_queue_2

--- iOS.Project/3. Dispatch Queue/Dispatch_Queue/Dispatch_Queue/ViewController.swift
+++ iOS.Project/3. Dispatch Queue/Dispatch_Queue/Dispatch_Queue/ViewController.swift
@@ -1,4 +1,4 @@
-// dispatch queue
+// dispatch queue -> like c# of task
 // create thread's' for manage
 
 import UIKit
@@ -20,11 +20,15 @@ class ViewController: UIViewController {
     }
     
     // run on main thread
+    // why ui job work in main thread?
+    // -> when ui state updated, one thread has to manage that.
+    // if multi thread manage that, you hardly to check this.
+    // And app life cycle run on main thread.
     @IBAction func action1(_ sender: Any) {
         // finishLabel.text = "end"
-//        simpleClousure {
-//            finishLabel.text = "end_closure"
-//        }
+        // simpleClousure {
+        // finishLabel.text = "end_closure"
+        // }
         multiThreadClosure {
             
             self.finishLabel.text = "end_multThread"
@@ -57,6 +61,81 @@ class ViewController: UIViewController {
         // ui must run on main thread
         DispatchQueue.main.async {
             complection()
+        }
+    }
+    
+    
+    @IBAction func action2(_ sender: Any) {
+//        simpleAction2()
+        multiAction2()
+        
+    }
+    
+    // run on sync -> run on one thread
+    func simpleAction2() {
+        let dispatchGroup = DispatchGroup()
+        
+        // create a thread
+        let queue1 = DispatchQueue(label: "q1")
+        
+        queue1.async(group: dispatchGroup) {
+            for index in 0..<10 {
+                Thread.sleep(forTimeInterval: 0.2)
+                print(index)
+            }
+        }
+        
+        // run on sync -> run on one thread
+        queue1.async(group: dispatchGroup) {
+            for index in 10..<20 {
+                Thread.sleep(forTimeInterval: 0.2)
+                print(index)
+            }
+        }
+        
+        // run on sync -> run on one thread
+        queue1.async(group: dispatchGroup) {
+            for index in 20..<30 {
+                Thread.sleep(forTimeInterval: 0.2)
+                print(index)
+            }
+        }
+    }
+    
+    // run on multi threads
+    // run on async
+    func multiAction2() {
+        let dispatchGroup = DispatchGroup()
+        
+        // create multi threads
+        let queue1 = DispatchQueue(label: "q1")
+        let queue2 = DispatchQueue(label: "q2")
+        let queue3 = DispatchQueue(label: "q3")
+
+        queue1.async(group: dispatchGroup) {
+            for index in 0..<10 {
+                Thread.sleep(forTimeInterval: 0.2)
+                print(index)
+            }
+        }
+        
+        queue2.async(group: dispatchGroup) {
+            for index in 10..<20 {
+                Thread.sleep(forTimeInterval: 0.2)
+                print(index)
+            }
+        }
+        
+        queue3.async(group: dispatchGroup) {
+            for index in 20..<30 {
+                Thread.sleep(forTimeInterval: 0.2)
+                print(index)
+            }
+        }
+        
+        // if all threads work finished, excute code block
+        dispatchGroup.notify(queue: DispatchQueue.main) {
+            print("end")
         }
     }
 }
