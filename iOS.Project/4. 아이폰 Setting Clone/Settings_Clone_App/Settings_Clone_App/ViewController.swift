26f8681395552e7a215bc1e5db371a11065c0a1e
Joseph.c <joseph.c@kakao.com>
2022-11-19 21:59:18 +0900

    #63 아이폰 Setting Clone_1

---
+++ iOS.Project/4. 아이폰 Setting Clone/Settings_Clone_App/Settings_Clone_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Settings_Clone_App
+//
+//  Created by 차동훈 on 2022/11/19.
+//
+
+import UIKit
+
+// 화면의 UI를 표시, 화면 이동을 관리함
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
