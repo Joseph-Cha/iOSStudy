234b451d489483f3baae2c5eb9d03d0e3a1759a4
Joseph.c <joseph.c@kakao.com>
2022-12-01 23:41:03 +0900

    #74 PanGesture_1_(프로젝트 생성)

--- iOS.Project/4. 아이폰 Setting Clone/Settings_Clone_App/Settings_Clone_App/ViewController.swift
+++ iOS.Project/4. 아이폰 Setting Clone/Settings_Clone_App/Settings_Clone_App/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Settings_Clone_App
-//
-//  Created by 차동훈 on 2022/11/19.
-//
-
 import UIKit
 
 // 화면의 UI를 표시, 화면 이동을 관리함
---
+++ iOS.Project/5. Pan Gesture/PanGestureApp/PanGestureApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PanGestureApp
+//
+//  Created by 차동훈 on 2022/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
