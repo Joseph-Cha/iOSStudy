f2e94276238736f312a4f68003787afdfd5796e0
Joseph.c <joseph.c@kakao.com>
2022-12-19 20:53:51 +0900

    #96 TodoApp_3_(fetch data, table view 연동)

--- iOS.Project/9. To Do App/TodoList/TodoList/ViewController.swift
+++ iOS.Project/9. To Do App/TodoList/TodoList/ViewController.swift
@@ -6,10 +6,15 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var todoTableView: UITableView!
+    let appdelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    var todoList = [TodoList]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Todo List"
@@ -17,6 +22,22 @@ class ViewController: UIViewController {
         
         todoTableView.dataSource = self
         todoTableView.delegate = self
+        
+        fetchDate()
+        todoTableView.reloadData()
+    }
+    
+    // core date를 가지고 온다.
+    func fetchDate() {
+        let fetchRequest: NSFetchRequest<TodoList> = TodoList.fetchRequest()
+        let context = appdelegate.persistentContainer.viewContext
+        
+        do {
+            self.todoList = try context.fetch(fetchRequest)
+            
+        } catch {
+            print(error)
+        }
     }
         
     func makeNavigationBar() {
@@ -36,11 +57,22 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return self.todoList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath) as! TodoCelll
+        cell.topTitleLabel.text = todoList[indexPath.row].title
+        if let hasDate = todoList[indexPath.row].date {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "MM-dd hh:mm:ss"
+            let dateString = formatter.string(from: hasDate)
+            
+            cell.dateLabel.text = dateString
+        } else {
+            cell.dateLabel.text = ""
+        }
+        
         return cell
     }
     
